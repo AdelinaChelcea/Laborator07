ddd4525c5eb6f92b32583b55de3b0710fa6ecb87
Student User <student@cti.pub.ro>
2017-04-25 09:44:31 +0300

    ex4 + ex5(patial)

--- labtasks/CalculatorWebService/app/src/main/java/ro/pub/cs/systems/eim/lab07/calculatorwebservice/network/CalculatorWebServiceAsyncTask.java
+++ labtasks/CalculatorWebService/app/src/main/java/ro/pub/cs/systems/eim/lab07/calculatorwebservice/network/CalculatorWebServiceAsyncTask.java
@@ -1,7 +1,27 @@
 package ro.pub.cs.systems.eim.lab07.calculatorwebservice.network;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.protocol.HTTP;
+import ro.pub.cs.systems.eim.lab07.calculatorwebservice.R;
+import ro.pub.cs.systems.eim.lab07.calculatorwebservice.general.Constants;
 
 public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, String> {
 
@@ -17,19 +37,58 @@ public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, Strin
         String operator2 = params[1];
         String operation = params[2];
         int method = Integer.parseInt(params[3]);
-
+        String content = null;
         // TODO exercise 4
         // signal missing values through error messages
 
         // create an instance of a HttpClient object
 
         // get method used for sending request from methodsSpinner
+        HttpClient httpClient = new DefaultHttpClient();
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        if(method == Constants.GET_OPERATION) {
+            // 1. GET
+            // a) build the URL into a HttpGet object (append the operators / operations to the Internet address)
+            // b) create an instance of a ResultHandler object
+            // c) execute the request, thus generating the result
 
-        // 1. GET
-        // a) build the URL into a HttpGet object (append the operators / operations to the Internet address)
-        // b) create an instance of a ResultHandler object
-        // c) execute the request, thus generating the result
+            HttpGet httpGet = new HttpGet(Constants.GET_WEB_SERVICE_ADDRESS
+                    + "?" + Constants.OPERATION_ATTRIBUTE + "=" + operation
+                    + "&" + Constants.OPERATOR1_ATTRIBUTE + "=" + operator1
+                    + "&" + Constants.OPERATOR2_ATTRIBUTE + "=" + operator2);
 
+
+            try {
+                content = httpClient.execute(httpGet, responseHandler);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        else{
+            HttpPost httpPost = new HttpPost(Constants.POST_WEB_SERVICE_ADDRESS);
+            List<NameValuePair> paramss = new ArrayList<NameValuePair>();
+            paramss.add(new BasicNameValuePair(Constants.OPERATION_ATTRIBUTE, operation));
+            paramss.add(new BasicNameValuePair(Constants.OPERATOR1_ATTRIBUTE, operator1));
+            paramss.add(new BasicNameValuePair(Constants.OPERATOR2_ATTRIBUTE, operator2));
+            try {
+                UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(paramss, HTTP.UTF_8);
+                httpPost.setEntity(urlEncodedFormEntity);
+            } catch (UnsupportedEncodingException unsupportedEncodingException) {
+                Log.e(Constants.TAG, unsupportedEncodingException.getMessage());
+                if (Constants.DEBUG) {
+                    unsupportedEncodingException.printStackTrace();
+                }
+            }
+
+            try {
+                content = httpClient.execute(httpPost, responseHandler);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         // 2. POST
         // a) build the URL into a HttpPost object
         // b) create a list of NameValuePair objects containing the attributes and their values (operators, operation)
@@ -37,12 +96,14 @@ public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, Strin
         // d) create an instance of a ResultHandler object
         // e) execute the request, thus generating the result
 
-        return null;
+        return content;
     }
 
     @Override
     protected void onPostExecute(String result) {
         // display the result in resultTextView
+        this.resultTextView.setText(result);
+
     }
 
 }
--- labtasks/XKCDCartoonDisplayer/app/src/main/java/ro/pub/cs/systems/eim/lab07/xkcdcartoondisplayer/network/XKCDCartoonDisplayerAsyncTask.java
+++ labtasks/XKCDCartoonDisplayer/app/src/main/java/ro/pub/cs/systems/eim/lab07/xkcdcartoondisplayer/network/XKCDCartoonDisplayerAsyncTask.java
@@ -1,12 +1,30 @@
 package ro.pub.cs.systems.eim.lab07.xkcdcartoondisplayer.network;
 
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import ro.pub.cs.systems.eim.lab07.xkcdcartoondisplayer.entities.XKCDCartoonInformation;
+import ro.pub.cs.systems.eim.lab07.xkcdcartoondisplayer.general.Constants;
 
 public class XKCDCartoonDisplayerAsyncTask extends AsyncTask<String, Void, XKCDCartoonInformation> {
 
@@ -45,10 +63,47 @@ public class XKCDCartoonDisplayerAsyncTask extends AsyncTask<String, Void, XKCDC
         // TODO exercise 5a)
         // 1. obtain the content of the web page (whose Internet address is stored in urls[0])
         // - create an instance of a HttpClient object
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(urls[0]);
+        String content = null;
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        HttpResponse responseImage = null;
+        InputStream imageStream = null;
+        try {
+             content = httpClient.execute(httpGet, responseHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         // - create an instance of a HttpGet object
         // - create an instance of a ResponseHandler object
         // - execute the request, thus obtaining the web page source code
 
+        if (content != null) {
+            Document document = Jsoup.parse(content);
+            Element htmlTag = document.child(0);
+            Element divTagIdCtitle = htmlTag.getElementsByAttributeValue(Constants.ID_ATTRIBUTE, Constants.CTITLE_VALUE).first();
+            xkcdCartoonInformation.setCartoonTitle(divTagIdCtitle.ownText());
+
+            Element divTagIdComic = htmlTag.getElementsByAttributeValue(Constants.ID_ATTRIBUTE, Constants.COMIC_VALUE).first();
+            String cartoonInternetAddress = divTagIdComic.getElementsByTag(Constants.IMG_TAG).attr(Constants.SRC_ATTRIBUTE);
+
+            HttpGet httpGetImage = new HttpGet(cartoonInternetAddress);
+            try {
+                responseImage = httpClient.execute(httpGetImage);
+                imageStream = responseImage.getEntity().getContent();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            xkcdCartoonInformation.setCartoonBitmap(BitmapFactory.decodeStream(imageStream));
+
+            Element aTagRelPrev = htmlTag.getElementsByAttributeValue(Constants.REL_ATTRIBUTE, Constants.PREVIOUS_VALUE).first();
+            String previousCartoonInternetAddress = Constants.XKCD_INTERNET_ADDRESS + aTagRelPrev.attr(Constants.HREF_ATTRIBUTE);
+
+            Element aTagRelNext = htmlTag.getElementsByAttributeValue(Constants.REL_ATTRIBUTE, Constants.NEXT_VALUE).first();
+            String nextCartoonInternetAddress = Constants.XKCD_INTERNET_ADDRESS + aTagRelNext.attr(Constants.HREF_ATTRIBUTE);
+        }
         // 2. parse the web page source code
         // - cartoon title: get the tag whose id equals "ctitle"
         // - cartoon url
@@ -81,12 +136,18 @@ public class XKCDCartoonDisplayerAsyncTask extends AsyncTask<String, Void, XKCDC
         // TODO exercise 5b)
         // map each member of xkcdCartoonInformation object to the corresponding widget
         // cartoonTitle -> xkcdCartoonTitleTextView
+        xkcdCartoonTitleTextView.setText(xkcdCartoonInformation.getCartoonTitle());
         // cartoonBitmap -> xkcdCartoonImageView (only if using Apache HTTP Components)
+        xkcdCartoonImageView.setImageBitmap(xkcdCartoonInformation.getCartoonBitmap());
         // cartoonUrl -> xkcdCartoonUrlTextView
+        xkcdCartoonUrlTextView.setText(xkcdCartoonInformation.getCartoonUrl());
         // based on cartoonUrl fetch the bitmap using Volley (using an ImageRequest object added to the queue)
         // and put it into xkcdCartoonImageView
         // previousCartoonUrl, nextCartoonUrl -> set the XKCDCartoonUrlButtonClickListener for previousButton, nextButton
-
+        XKCDCartoonButtonClickListener buttonListenerPrev = new XKCDCartoonButtonClickListener(xkcdCartoonInformation.getPreviousCartoonUrl());
+        XKCDCartoonButtonClickListener buttonListenerNext= new XKCDCartoonButtonClickListener(xkcdCartoonInformation.getNextCartoonUrl());
+        buttonListenerPrev.onClick(previousButton);
+        buttonListenerNext.onClick(nextButton);
     }
 
 }
